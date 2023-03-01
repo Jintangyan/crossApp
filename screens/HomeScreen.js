d34327dd0ff7d7e1e911119b310e0b6b76e007ef
Jintang Yan <9180@ait.nsw.edu.au>
2023-03-01 19:46:29 +1100

    added screens and navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from "react-native"
+
+export function HomeScreen (props){
+    return(
+        <view>
+            <Text>Home Screen</Text>
+        </view>
+    )
+}
---
+++ screens/SignUp.js
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from "react-native"
+
+export function SignUpScreen (props){
+    return(
+        <view>
+            <Text>Sign up Screen</Text>
+        </view>
+    )
+}
